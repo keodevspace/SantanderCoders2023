5952225b47f4c32c57468bbe3342098a7183cd99
Keo Coelho de Almeida <keo.almeida@totvs.com.br>
2024-01-19 17:18:38 -0300

    JSON and JS done \o/

--- front-end-estatico/aula-5/coffee-shop/home.js
+++ front-end-estatico/aula-5/coffee-shop/home.js
@@ -1,18 +1,47 @@
-const rangeInput = document.getElementById('nivel-satisfacao');
-const porcentagemText = document.getElementById('porcentagem');
+// evento para receber nivel de satisfação
+const rangeInput = document.getElementById("nivel-satisfacao");
+const porcentagemText = document.getElementById("porcentagem");
 
-rangeInput.addEventListener('input', () => {
-    porcentagemText.textContent = `${rangeInput.value}%`;
-  });
+rangeInput.addEventListener("input", () => {
+  porcentagemText.textContent = `${rangeInput.value}%`;
+});
 
-  // revisar
-  const enviarButton = document.querySelector('button');
-  enviarButton.addEventListener('click', () => {      
-      console.log('Formulário enviado!');
-  });
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // valores dos campos do formulário
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const ideia = document.getElementById("ideia").value;
+  const porcentagem = document.getElementById("porcentagem").textContent; // Corrigido aqui
+
+  // objeto com os dados do participante
+  const participantData = {
+    name,
+    email,
+    ideia,
+    porcentagem,
+  };
+
+  // chamada da API usando fetch
+  fetch("http://localhost:3000/participants", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(participantData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Ideia Brilhante! Em breve retornaremos o contato!", data);
+      alert("Ideia Brilhante! Em breve retornaremos o contato!");
+    })
+    .catch((error) => {
+      console.error("Ops...Erro ao registrar sua ideia. Tente novamente!", error);
+      alert("Ops...Erro ao registrar sua ideia. Tente novamente!");
+    });
+});
 
 function toggleDescription(card) {
-    card.classList.toggle('active');
+  card.classList.toggle("active");
 }
-  
- 
